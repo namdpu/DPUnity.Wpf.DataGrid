a7ad0834962bdc4aa42ec7d55b827a1ef7ac836f
LKV <lakhavanzz@gmail.com>
2025-08-19 15:24:46 +0700

    Update version and enhance FilterDataGrid styles
    - Bump version from 1.0.8 to 1.0.10 in project file.
    - Introduce new style properties for FilterDataGrid in Generic.xaml.
    - Add triggers for dynamic visibility based on SortDirection.
    - Remove DP_DataGridRowBaseStyle for style consolidation.
    - Add SelectedItemsBehavior to manage DataGrid selected items.

---
+++ src/Behaviors/SelectedItemsBehavior.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Media;
+
+using SystemDataGrid = System.Windows.Controls.DataGrid;
+
+namespace DPUnity.Wpf.DpDataGrid.Behaviors
+{
+    public static class SelectedItemsBehavior
+    {
+        public static readonly DependencyProperty SelectedItemsProperty =
+            DependencyProperty.RegisterAttached(
+                "SelectedItems",
+                typeof(IList),
+                typeof(SelectedItemsBehavior),
+                new PropertyMetadata(null, OnSelectedItemsChanged));
+        public static IList GetSelectedItems(DependencyObject obj)
+        {
+            return (IList)obj.GetValue(SelectedItemsProperty);
+        }
+        public static void SetSelectedItems(DependencyObject obj, IList value)
+        {
+            obj.SetValue(SelectedItemsProperty, value);
+        }
+        private static void OnSelectedItemsChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is SystemDataGrid dataGrid)
+            {
+                dataGrid.SelectionChanged -= DataGrid_SelectionChanged;
+                if (e.NewValue is IList newList)
+                {
+                    dataGrid.SelectionChanged += DataGrid_SelectionChanged;
+                    dataGrid.SelectedItems.Clear();
+                    foreach (var item in newList)
+                    {
+                        dataGrid.SelectedItems.Add(item);
+                    }
+                }
+            }
+        }
+        private static void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (sender is SystemDataGrid dataGrid && GetSelectedItems(dataGrid) is IList selectedItems)
+            {
+                selectedItems.Clear();
+                foreach (var item in dataGrid.SelectedItems)
+                {
+                    selectedItems.Add(item);
+                }
+            }
+        }
+    }
+}
